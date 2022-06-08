75ec2fee20f0fe44167909f958b19cd3146c1713
Nabil Arta <thenabilarta@gmail.com>
2022-06-08 15:51:29 +0700

    Finishing edit profile screen

--- twitter/Controller/ProfileController.swift
+++ twitter/Controller/ProfileController.swift
@@ -193,7 +193,11 @@ extension ProfileController: ProfileHeaderDelegate {
     func handleEditProfileFollow(_ header: ProfileHeader) {
         
         if user.isCurrentUser {
-            print("Show edit profile controller")
+            let controller = EditProfileController(user: user)
+            controller.delegate = self
+            let nav = UINavigationController(rootViewController: controller)
+            nav.modalPresentationStyle = .fullScreen
+            present(nav, animated: true)
             return
         }
         
@@ -217,6 +221,16 @@ extension ProfileController: ProfileHeaderDelegate {
     
     func handleDismissal() {
         navigationController?.popViewController(animated: true)
+    }
+}
+
+// MARK: - EditProfileControllerDelegate
+
+extension ProfileController: EditProfileControllerDelegate {
+    func controller(_ controller: EditProfileController, wantsToUpdate user: User) {
+        controller.dismiss(animated: true)
+        self.user = user
+        self.collectionView.reloadData()
     }
 }
 
--- twitter/View/Profile/ProfileHeader.swift
+++ twitter/View/Profile/ProfileHeader.swift
@@ -229,6 +229,7 @@ class ProfileHeader: UICollectionReusableView {
         
         fullnameLabel.text = user.fullname
         usernameLabel.text = viewModel.usernameText
+        bioLabel.text = user.bio
     }
     
 }
